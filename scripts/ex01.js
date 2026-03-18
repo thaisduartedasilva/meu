7d82a09932f85c27963266f7a1587308e4a0886c
ThaisDuarte <thais_duarte-silva@estudante.sesisenai.org.br>
2026-03-18 12:49:09 -0300

    add - ex01

---
+++ scripts/ex01.js
@@ -0,0 +1,16 @@
+function somarNumeros( ) {
+    let soma = 0;
+    let continuar = true;
+    while (continuar) {
+        let numero = parseFloat(prompt("Digite um número para somar: "));
+        if (!isNaN(numero)) {
+            soma += numero;
+            console.log(soma);
+    } else {
+        alert("Por favor, digite um número válido!");
+    }
+    continuar = confirm("Deseja adicionar mais um número?");
+    
+    }
+    alert("A soma dos números é: " + soma);
+}
