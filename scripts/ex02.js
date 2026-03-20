fefa36e9d9395eb30cdb11326a600d2cd2a203fe
thais <thais_duarte-silva@estudante.sesisenai.org.br>
2026-03-20 12:10:14 -0300

    add - ex03

---
+++ scripts/ex02.js
@@ -0,0 +1,10 @@
+function NASA() {
+
+    for (let i = 10; i >= 0; i--){
+       console.log(i) 
+       alert(i);
+    }
+
+    alert("Lançamento realizado!");
+
+}
---
+++ scripts/ex03.js
@@ -0,0 +1,17 @@
+function financeiro(){
+    let ganho;
+    let gasto;
+    let continuar = true;
+
+    for (let i = 1; i >= 12; i++){
+        let ganho = parseFloat(prompt("Informe o ganho bruto do mês: "));
+        if (!isNaN(numero)) {
+            soma += numero;
+            console.log(soma);
+        } else {
+            alert("Por favor, digite um número válido!");
+        }
+        continuar = confirm("Deseja adicionar mais um número?");
+    }
+
+}
