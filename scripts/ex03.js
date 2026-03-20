06fb7de9d0171e8d85952caa6057b5aec5c7ab61
thais <thais_duarte-silva@estudante.sesisenai.org.br>
2026-03-20 12:21:48 -0300

    add - ex03_tentando

--- scripts/ex03.js
+++ scripts/ex03.js
@@ -1,17 +1,25 @@
-function financeiro(){
+function financeiro() {
     let ganho;
     let gasto;
-    let continuar = true;
+    let ganhoB;
+
 
     for (let i = 1; i >= 12; i++){
         let ganho = parseFloat(prompt("Informe o ganho bruto do mês: "));
-        if (!isNaN(numero)) {
-            soma += numero;
-            console.log(soma);
+        if (!isNaN(ganho)) {
+            ganhoB += ganho;
         } else {
             alert("Por favor, digite um número válido!");
         }
-        continuar = confirm("Deseja adicionar mais um número?");
+    }
+
+    for (let i = 1; i >= 12; i++){
+    let gasto = parseFloat(prompt("Informe o gasto do mês: "));
+    if (!isNaN(gasto)) {
+        ganhoB += gasto;
+    } else {
+            alert("Por favor, digite um número válido!");
+        }
     }
 
 }
