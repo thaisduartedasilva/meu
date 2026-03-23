64e3dd7268cf397c1bfe9fffc0a2688dd732a797
ThaisDuarte <thais_duarte-silva@estudante.sesisenai.org.br>
2026-03-23 12:59:47 -0300

    add - ex03

--- scripts/ex03.js
+++ scripts/ex03.js
@@ -1,25 +1,39 @@
 function financeiro() {
-    let ganho;
-    let gasto;
-    let ganhoB;
+    let ganhoAnual;
+    let gastoAnual;
 
-
-    for (let i = 1; i >= 12; i++){
+    for (let i = 1; i <= 12; i++) {
         let ganho = parseFloat(prompt("Informe o ganho bruto do mês: "));
+        let gasto = parseFloat(prompt("Informe o gasto do mês: "));
+
         if (!isNaN(ganho)) {
-            ganhoB += ganho;
+            ganhoAnual += ganho;
+        } else {
+            alert("Por favor, digite um número válido!");
+        }
+
+        if (!isNaN(gasto)) {
+            gastoAnual += gasto;
         } else {
             alert("Por favor, digite um número válido!");
         }
     }
 
-    for (let i = 1; i >= 12; i++){
-    let gasto = parseFloat(prompt("Informe o gasto do mês: "));
-    if (!isNaN(gasto)) {
-        ganhoB += gasto;
-    } else {
-            alert("Por favor, digite um número válido!");
-        }
+    alert ("O ganho bruto anual da sua empresa foi de: " + ganhoAnual);
+    alert ("O gasto anual da sua empresa foi de: " + gastoAnual);
+
+    let saldo = ganhoAnual - gastoAnual; 
+
+    alert ("O saldo da sua empresa foi de: " + saldo);
+
+    if (saldo >= 0) {
+        alert ("A empresa teve lucro.");
+    }else{
+        alert ("A empresa teve prejuízo");
     }
+
+
+
+
 
 }
