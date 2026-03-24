a7b8efa8b38a50ae31b85a7ea608f05de14a64a3
ThaisDuarte <thais_duarte-silva@estudante.sesisenai.org.br>
2026-03-24 16:42:53 -0300

    add - ex09

---
+++ scripts/ex09.js
@@ -0,0 +1,10 @@
+function doisNumeros(){
+    let numero1 = 89;
+    let numero2 = 39;
+
+    if (numero1 > numero2) {
+        alert (numero1 + ", " + numero2);
+    } else {
+        alert (numero2 + ", " + numero1);
+    }
+}
