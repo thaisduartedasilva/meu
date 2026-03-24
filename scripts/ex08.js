2d9dc74ca646dd80a084e9ff8bc20c5cb27ad2f9
ThaisDuarte <thais_duarte-silva@estudante.sesisenai.org.br>
2026-03-24 15:46:20 -0300

    add - ex08

---
+++ scripts/ex08.js
@@ -0,0 +1,16 @@
+function variaveis(){
+    let numero1 = 6;
+    let numero2 = 7; 
+    let diferenca;
+    let B; 
+    let mult;
+
+    diferenca = numero1 - numero2;
+    B = (numero1 * 2) + (numero2 * 3);
+    mult = numero1*numero2;
+
+    alert ("A diferença entre os 2 números é: " + diferenca);
+    alert ("O dobro do primeiro número somado ao triplo do segundo é: " + B);
+    alert ("A multiplicação dos 2 números é: " + mult);
+
+}
